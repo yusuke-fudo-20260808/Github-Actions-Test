22752590b266525984bdb224ffc5d6b994a911b3
yuusuke-fudou-worker <yuusuke-fudou@navio.co.jp>
2026-08-09 16:43:33 +0900

    Add Terraform configuration for Azure resources

---
+++ codes/Dev/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "sttfstate20260809"
+    container_name       = "tfstate"
+    key                  = "github-actions-test.tfstate"
+
+    use_oidc = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "rg-github-actions-test"
+  location = "Japan West"
+}
