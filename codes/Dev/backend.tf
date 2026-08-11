65df2bfdad4fa06e26f01357042a95cd9a1f7585
yuusuke-fudou-worker <yuusuke-fudou@navio.co.jp>
2026-08-11 11:48:21 +0900

    Update backend.tf

--- codes/Dev/backend.tf
+++ codes/Dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-psynaps-prd"
+    resource_group_name  = "rg-psynaps-dev"
     storage_account_name = "sttfstate20260810"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
