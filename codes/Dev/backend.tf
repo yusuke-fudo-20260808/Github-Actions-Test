56fbd6fa337f17b28ce754f1fb464a4e9cee29e6
youthk800 <youth.k800@gmail.com>
2026-08-11 13:55:58 +0900

    Update backend.tf

--- codes/Dev/backend.tf
+++ codes/Dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-psynaps-prd"
-    storage_account_name = "stbackuppsynapsprd"
+    storage_account_name = "sttfstate20260810"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
