6206f4ee3b715bfc523ec8a1843934d06c16193d
youthk800 <youth.k800@gmail.com>
2026-08-11 13:49:16 +0900

    Add files via upload

--- codes/Dev/backend.tf
+++ codes/Dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-psynaps-dev"
-    storage_account_name = "sttfstate20260810"
+    resource_group_name  = "rg-psynaps-prd"
+    storage_account_name = "stbackuppsynapsprd"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
