0f56a6ac9315117eb489f271e654f332cd5e9d3b
yuusuke-fudou-worker <yuusuke-fudou@navio.co.jp>
2026-08-11 11:52:08 +0900

    Comment out unused variables in variables.tf
    Commented out unused variables in variables.tf.

--- codes/Dev/variables.tf
+++ codes/Dev/variables.tf
@@ -3,22 +3,22 @@ variable "subscription_id" {
   type        = string
 }
 
-variable "resource_group_name" {
-  description = "リソースグループ名"
-  type        = string
-}
+#variable "resource_group_name" {
+#  description = "リソースグループ名"
+#  type        = string
+#}
 
-variable "location" {
-  description = "Azureリージョン"
-  type        = string
-}
+#variable "location" {
+#  description = "Azureリージョン"
+#  type        = string
+#}
 
 #variable "vnet_name" {
 #  description = "仮想ネットワーク名"
 #  type        = string
 #}
 
-variable "environment" {
-  description = "環境識別子（dev/test/prod）"
-  type        = string
-}
+#variable "environment" {
+#  description = "環境識別子（dev/test/prod）"
+#  type        = string
+#}
