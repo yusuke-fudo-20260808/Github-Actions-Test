04e71b2f1561c468af82fd809edbe9109d69a98e
youthk800 <youth.k800@gmail.com>
2026-08-31 17:19:59 +0900

    Add files via upload

---
+++ codes/Dev/container_registry.tf
@@ -0,0 +1,93 @@
+locals {
+  acr_tags = merge(
+    local.common_tags,
+    {
+      Env                = var.environment
+      DataClassification = "Confidential"
+      System             = "PSYNAPS"
+      Criticality        = "High"
+    }
+  )
+}
+
+# ========================================================================
+# 1. Azure Container Registry リソース
+# ========================================================================
+
+resource "azurerm_container_registry" "aca" {
+  name                = "acrpsynapsdev"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = "Premium"
+
+  admin_enabled                 = false
+  anonymous_pull_enabled        = false
+  public_network_access_enabled = false
+  data_endpoint_enabled         = false
+  zone_redundancy_enabled       = false
+
+  network_rule_bypass_option = "AzureServices"
+
+  tags = local.acr_tags
+}
+
+# ========================================================================
+# 2. Private DNSゾーン リソース
+# ========================================================================
+
+resource "azurerm_private_dns_zone" "container_registry" {
+  name                = "privatelink.azurecr.io"
+  resource_group_name = var.resource_group_name
+
+  tags = local.acr_tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "container_registry" {
+  name                  = "pdnslink-acr-psynaps-dev"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.container_registry.name
+  virtual_network_id    = data.azurerm_virtual_network.this.id
+  registration_enabled  = false
+
+  tags = local.acr_tags
+}
+
+# ========================================================================
+# 3. プライベートエンドポイント リソース
+# ========================================================================
+
+resource "azurerm_private_endpoint" "aca_registry" {
+  name                = "pep-acr-psynaps-dev"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = azurerm_subnet.dev["dev"].id
+
+  private_service_connection {
+    name                           = "psc-acr-psynaps-dev"
+    private_connection_resource_id = azurerm_container_registry.aca.id
+    subresource_names              = ["registry"]
+    is_manual_connection           = false
+  }
+
+  ip_configuration {
+    name               = "prip-acr-psynaps-dev-01"
+    private_ip_address = "10.14.167.12"
+    subresource_name   = "registry"
+    member_name        = "registry"
+  }
+
+  ip_configuration {
+    name               = "prip-acr-psynaps-dev-02"
+    private_ip_address = "10.14.167.19"
+    subresource_name   = "registry"
+    member_name        = "registry_data_japaneast"
+  }
+
+  private_dns_zone_group {
+    name                 = "pdnsgroup-acr-psynaps-dev"
+    private_dns_zone_ids = [azurerm_private_dns_zone.container_registry.id]
+  }
+
+  tags = local.acr_tags
+}
+
