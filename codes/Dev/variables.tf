0fda678d678c5a35ade65c15320f091af6e36b9b
youthk800 <youth.k800@gmail.com>
2026-09-01 11:02:15 +0900

    Update variables.tf

--- codes/Dev/variables.tf
+++ codes/Dev/variables.tf
@@ -3,11 +3,6 @@ variable "subscription_id" {
   type        = string
 }
 
-variable "resource_group_name" {
-  description = "リソースグループ名"
-  type        = string
-}
-
 variable "location" {
   description = "Azureリージョン"
   type        = string
